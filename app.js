099cdb0c75817545ac35e51b29bcfbea65e32737
Fredrik Reveny <fredrik.reveny@mycronic.com>
2016-08-10 14:29:08 +0200

    Added support for non-Chetics

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var requestify = require('requestify');
 
-requestify.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=Chetic&api_key=51de025812af79cb70f4a872936181a0&format=json&limit=1')
-  .then(function(response) {
-      json = response.getBody();
-      console.log(json.recenttracks.track[0].artist['#text'] + ' - ' + json.recenttracks.track[0].name);
-  }
-);
-
+if (process.argv.length >= 3) {
+	requestify.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + process.argv[2] + '&api_key=51de025812af79cb70f4a872936181a0&format=json&limit=1')
+		.then(function(response) {
+			json = response.getBody();
+			console.log(json.recenttracks.track[0].artist['#text'] + ' - ' + json.recenttracks.track[0].name);
+		}
+	);
+}
